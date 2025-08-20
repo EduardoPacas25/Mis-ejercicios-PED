f80273867e899782ce8ba838eb66ea7b1dfd9ae3
EduardoPacas25 <00134525@uca.edu.sv>
2025-08-20 12:42:47 -0600

    :sparkles: feat(cupones): Añadir entrada y verificación de las letras para los cupones

--- clase_1.cc
+++ clase_1.cc
@@ -2,40 +2,70 @@
 #include <ctime>
 #include <cstdlib>
 #include <string>
+#include <cctype>
 
+std::string SolicitarDatos();
 std::string GenerarCupon (std::string letras);
 void VerificarPremio (std::string cupon);
 
 int main(){
-  std::string letras;
-  std::string cupon_generado;
+  int cantidad_cupones = 0;
 
-  std::cout<<"? \n";
-  std::cin>>letras;
+  std::cout<<"Ingrese la cantidad de cupones que desea generar \n";
+  std::cin>>cantidad_cupones;
+  std::string cupones[cantidad_cupones];
 
-  cupon_generado = GenerarCupon(letras);
-  std::cout<< cupon_generado <<std::endl;
+  for (int i = 0; i < cantidad_cupones; i++){
+    std::string letras;
+    std::string cupon_generado;
 
-  VerificarPremio(cupon_generado);
+    letras = SolicitarDatos();
+
+    cupon_generado = GenerarCupon(letras);
+    std::cout<<"El cupon generado es: "<< cupon_generado <<std::endl;
+
+    VerificarPremio(cupon_generado);
+    cupones[i]=cupon_generado;
+  }
+  std::cout<<"Cupones generados: \n";
+    for (int i = 0; i < cantidad_cupones; i++){
+    std::cout<<cupones[i] <<std::endl;
+  }
+  
 
   return 0;
 }
 
+std::string SolicitarDatos(){
+  //Solicitar el prefijo
+  std::string prefijo;
+  std::cout<<"Ingrese las letras para el prefijo de su cupon \n";
+  std::cout<<"(Debe tener un maximo de 3 letras) \n";
+  std::cin>>prefijo;
+   while(prefijo.size() != 3){
+    //Validar que la longitud length() sea 3
+    std::cout<<"Recuerde que deben ser 3 letras \n";
+    std::cin>>prefijo;
+  }
+  // que sea letra isalpha()
+  //return el prefijo validado
+  return prefijo;
+}
+
 std::string GenerarCupon (std::string letras) {
   srand(time(0));
-  int numero_aleatorio = rand() % 999;
+  int numero_aleatorio = rand() % 100 + 999;
   std::string conversion = std::to_string(numero_aleatorio);  
   return letras + conversion;
 }
 
 void VerificarPremio (std::string cupon) {
   std::string numeros = cupon.substr(3,4);
-  int valor_cupon;
-  valor_cupon = std::stoi(numeros);
+  int valor_cupon = std::stoi(numeros);
   if (valor_cupon%2 == 0) {
-      std::cout<<"Tiene premio" <<std::endl;
+      std::cout<<"Tiene premio \n";
   }
   else{
-      std::cout<<"No tiene premio" <<std::endl;
+      std::cout<<"No tiene premio \n";
   }
 }
