13e055a38cfaacc6399c0ae869dcb0c8e690fe8d
EduardoPacas25 <00134525@uca.edu.sv>
2025-09-03 12:42:19 -0600

    :sparkles: feat(vector): Agregar vector para guardar más de un cantante

--- clase_2.cc
+++ clase_2.cc
@@ -17,20 +17,29 @@ struct InformacionCantante {
 };
 
 struct InformacionCantante SolicitarDatos();
-struct InformacionCantante cantante;
+//struct InformacionCantante cantante;
 void Imprimir(InformacionCantante cantante);
-void CalcularAntiguedad(int lanzamiento);
+int CalcularAntiguedad(int lanzamiento);
+std::vector<struct InformacionCantante> vector_cantantes;
 
 int main(){
+  char opc;
 
+  do{
+  struct InformacionCantante cantante;
   cantante = SolicitarDatos();
-  CalcularAntiguedad(cantante.album_cantante.fecha_lanzamiento);
+  vector_cantantes.push_back(cantante);
+  cantante.album_cantante.antiguedad_album = CalcularAntiguedad(cantante.album_cantante.fecha_lanzamiento);
   Imprimir(cantante);
+  std::cout<<"Desea registrar otro cantante? \n";
+  std::cin>>opc;
+  } while(opc== 's' || opc== 'S');
 
   return 0;
 }
 
 struct InformacionCantante SolicitarDatos(){
+  struct InformacionCantante cantante;
   int cantidad_canciones;
   std::cout<<"Ingrese el nombre del cantante \n";
   std::cin>> cantante.nombre_cantante;
@@ -44,7 +53,7 @@ struct InformacionCantante SolicitarDatos(){
   std::cin>> cantidad_canciones;
   for(int i = 0; i<cantidad_canciones; i++){
     std::string nombre_c;
-    std::cout<<"Ingrese el nombre de la cancion \n";
+    std::cout<<"Ingrese el nombre de la cancion #" <<i+1  <<"\n";
     std::cin>>nombre_c;
     cantante.album_cantante.canciones.push_back({nombre_c, i+1});
   }
@@ -52,16 +61,21 @@ struct InformacionCantante SolicitarDatos(){
 }
 
 void Imprimir(InformacionCantante cantante){
+  std::cout<< "///////////////////////// \n";
   std::cout<< "--Cantante: "<<cantante.nombre_cantante <<"\n";
   std::cout<< "--Nacionalidad: "<<cantante.nacionalidad_cantante<<"\n";
   std::cout<< "--Album: "<<cantante.album_cantante.nombre_album<<"\n";
   std::cout<< "--Año de lanzamiento: "<<cantante.album_cantante.fecha_lanzamiento<<"\n";
+
   for (auto c: cantante.album_cantante.canciones){
     std::cout<< c.second <<". " << c.first <<"\n";
   }
   std::cout<< "-- El album fue lanzado hace " <<cantante.album_cantante.antiguedad_album <<" años" <<"\n";
+  std::cout<< "///////////////////////// \n";
 }
 
-void CalcularAntiguedad(int lanzamiento){
-  cantante.album_cantante.antiguedad_album = 2025 -  cantante.album_cantante.fecha_lanzamiento;
+int CalcularAntiguedad(int lanzamiento){
+  struct InformacionCantante cantante;
+  cantante.album_cantante.antiguedad_album = 2025 -  lanzamiento;
+  return cantante.album_cantante.antiguedad_album;
 }
