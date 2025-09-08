c53db316cd1da8358151854be420ab09fa10f5f3
EduardoPacas25 <00134525@uca.edu.sv>
2025-09-08 12:47:15 -0600

    feat(intercambio): Agregar funciones para intercambiar los valores de dos enteros
    Se agregaron funciones para intercambiar los valores de dos enteros por valor y por referencia

--- clase_3.cc
+++ clase_3.cc
@@ -3,10 +3,13 @@
 void PasoPorValor (int n);
 void PasoPorPuntero (int *ptr);
 void PasoPorReferencia (int &n);
+// Ejercicio:
+void IntercambiarValores(int a, int b);
+void IntercambiarValoresPuntero(int *ptr_a, int *ptr_b);
 
 int main()
 {
-  int num = 100;
+  /*int num = 100;
 
   //int *ptr = &num;
 
@@ -16,6 +19,16 @@ int main()
   PasoPorPuntero(&num);
   PasoPorReferencia(num); //110
   std::cout<< "Valor de num: " << num <<"\n"; //110
+  */
+
+  int a = 10;
+  int b = 5;
+
+  std::cout<< "Valor de a: " << a << " - Valor de b: "<< b << "\n";
+  IntercambiarValores(a, b);
+  std::cout<< "Valor de a: " << a << " - Valor de b: "<< b << "\n";
+  IntercambiarValoresPuntero(&a, &b);
+  std::cout <<"Valor de a: "<< a <<" - Valor de b: "<< b <<"\n";
 
   return 0;
 }
@@ -32,4 +45,20 @@ void PasoPorPuntero (int *ptr){
 void PasoPorReferencia (int &n){
   n = n + 10;
   std::cout<< "Valor de n: " << n <<"\n";
+}
+
+void IntercambiarValores(int a, int b){
+  int temp;
+  temp = b;
+  b = a;
+  a = temp;
+  std::cout <<"Valor de a: "<< a <<" - Valor de b: "<< b <<"\n";
+}
+
+void IntercambiarValoresPuntero(int *ptr_a, int *ptr_b){ 
+  int temp;
+  temp = *ptr_b;
+  *ptr_b = *ptr_a;
+  *ptr_a = temp;
+  std::cout <<"Valor de a: "<< *ptr_a <<" - Valor de b: "<< *ptr_b <<"\n";
 }
